6c5c23d5445f2b95199711934e582a6b1f6af76f
王守虎 <sszu@qq.com>
2019-05-13 16:23:42 +0800

    添加os.Getenv注释

--- env_test.go
+++ env_test.go
@@ -8,6 +8,7 @@ import (
 
 func TestEnv(t *testing.T) {
 	var envValue string
-	envValue = os.Getenv("wshenv")//windows下环境变量更新，要重启电脑才生效。linux可即时更新
+	os.Setenv("wshenv","369")
+	envValue = os.Getenv("wshenv")//windows下环境变量更新，要重启电脑才生效。linux只在当前终端生效
 	fmt.Println(envValue)
 }
