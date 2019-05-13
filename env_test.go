072cec1c57c88ad845af6c8253c118f1326ca4bd
王守虎 <sszu@qq.com>
2019-05-13 16:09:16 +0800

    添加os.Getenv注释

--- env_test.go
+++ env_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestEnv(t *testing.T) {
 	var envValue string
-	envValue = os.Getenv("GOPATH")
+	envValue = os.Getenv("wshenv")//windows下环境变量更新，要重启电脑才生效。linux可即时更新
 	fmt.Println(envValue)
 }
