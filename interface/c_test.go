cb1c8868e086f705dc7f044f09cd9e49b8a8523e
mistin <sszu@qq.com>
2020-03-25 15:29:51 +0800

    添加测试实例

--- interface/c_test.go
+++ interface/c_test.go
@@ -1,12 +1,46 @@
 package _interface
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"moqikaka.com/X1/PlatformHelper/timeHelper"
 	"moqikaka.com/goutil/timeUtil"
 	"testing"
 )
 
+type AB struct {
+	C interface{}
+}
+
+type AB1 struct {
+	C string
+}
+
 func TestA(t *testing.T){
+	//测试json无科学计数转换
+	s:=`[{"C":65811045}]`
+	v := &[]AB{}
+	d := json.NewDecoder(bytes.NewReader([]byte(s)))
+	d.UseNumber()
+	err := d.Decode(&v)
+	if err != nil {
+		// 错误处理
+	}
+	fmt.Println((*v)[0].C)
+	return
+
+	abObj:=map[string]interface{}{}
+
+	json.Unmarshal([]byte(s),abObj)
+
+
+	fmt.Println(abObj["C"])
+
+
+	//fmt.Println(fmt.Sprintf("%.0f",abObj.C))
+
+	return
+	//时间格式化测试
 	fmt.Println(timeUtil.ToDateTimeString2(timeHelper.GetNowAddMinTime(10)))
 }
